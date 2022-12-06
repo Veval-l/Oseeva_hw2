a0798f419dc8228f703c78c3b31bbe5a05158869
Oseeva <fselti1168rus@gmail.com>
2022-12-07 00:28:40 +0700

    8 off

--- src/Main.java
+++ src/Main.java
@@ -38,15 +38,22 @@ public class Main {
         System.out.println(frog);
 
         var firstBoxerWeight = 78.2;
-        System.out.println(firstBoxerWeight);
+        System.out.println("Вес первого боксёра" + firstBoxerWeight);
         var secondBoxerWeight = 82.7;
-        System.out.println(secondBoxerWeight);
+        System.out.println("Вес второго боксёра" + secondBoxerWeight);
         var boxersWeight = firstBoxerWeight + secondBoxerWeight;
         System.out.println("Общий вес двух боксеров " + boxersWeight + " кг");
         var differenceWeight = secondBoxerWeight - firstBoxerWeight;
         System.out.println("Разница веса между двумя боксерами " + differenceWeight + " кг");
         var diffWeight = secondBoxerWeight % firstBoxerWeight;
         System.out.println("Разница веса между двумя боксерами " + diffWeight + " кг");
+
+        var allTime = 640;
+        System.out.println("Всего часов работы " + allTime);
+        var oneEmployeeTime = 8;
+        System.out.println("Каждый сотрудник отрабатывает " + oneEmployeeTime + " часов");
+        var allEmployee = allTime / oneEmployeeTime;
+        System.out.println("Если в компании работает " + allEmployee + " человек, то всего " + allTime + " часов работы может быть поделено между сотрудниками");
 
     }
 }
